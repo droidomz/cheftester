c6e23454f9e8f960fef7155004bb0c47849f33cf
Omar Munoz <droidomz@gmail.com>
2015-07-08 15:28:30 -0500

    adding the cookbooks

---
+++ .chef/cookbooks/apache2/metadata.rb
@@ -0,0 +1,7 @@
+name             'apache2'
+maintainer       'Omar Inc'
+maintainer_email 'droidomz@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures apache2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
